2cca55c985a5a985a2acade68d7614362addc7c0
ofu997 <ofu997@gmail.com>
2020-04-23 13:51:24 -0700

    load an input with query result

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
-import { token } from '../token.js';
+import { token } from './token.js';
 const axiosGHGQL = axios.create({
   baseURL: 'https://api.github.com/graphql',
   headers: {
@@ -10,14 +10,60 @@ const axiosGHGQL = axios.create({
   },
 });
 
+const GET_ORGANIZATION = `
+{
+  organization(login: "the-road-to-learn-react") {
+    name
+    url
+  }
+}
+`;
+
 const title = 'React GraphQL Github Client'; 
 
-function App() {
-  return (
-<div>
-  <h1>{ title }</h1>
-</div>
-  );
+class App extends React.Component {
+  state = {
+    path: 'the-road-to-learn-react/the-road-to-learn-react',
+    };
+    componentDidMount() {
+      this.onFetchFromGitHub();
+    }
+    onChange = event => {
+    this.setState({ path: event.target.value });
+    };
+    onSubmit = event => {
+    // fetch data
+    event.preventDefault();
+  };  
+  onFetchFromGitHub = () => {
+    axiosGHGQL
+    .post('', { query: GET_ORGANIZATION })
+    .then(result => console.log(result));
+  };
+
+  render() {
+    const { path } = this.state; 
+    return (
+      <div>
+        <h1>{ title }</h1>
+        <form onSubmit = { this.onSubmit }>
+          <label htmlFor = 'url'>
+            Show open issues for Github
+            <input
+              id="url"
+              type="text"
+              value={ path }
+              onChange={ this.onChange }
+              style={{ width: '300px' }}
+            />
+            <button type="submit">Search</button>
+          </label>
+        </form>
+        <hr />
+        {/* result here */}
+      </div>
+    );
+  }
 }
 
 export default App;
