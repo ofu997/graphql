eb86da1d9903c5a87c11af89e40b96c86d5a2ac8
ofu997 <ofu997@gmail.com>
2020-04-23 00:19:43 -0700

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { token } from '../token.js';
+const axiosGHGQL = axios.create({
+  baseURL: 'https://api.github.com/graphql',
+  headers: {
+    Authorization: `bearer ${ token }`,
+  },
+});
+
+const title = 'React GraphQL Github Client'; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div>
+  <h1>{ title }</h1>
+</div>
   );
 }
 
